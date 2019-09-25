99957f35e59b922970ed82d0aacd0d6ed3f5ddd0
HaveAGitGat <43864057+HaveAGitGat@users.noreply.github.com>
2019-09-25 21:53:18 +0100

    init

---
+++ Tdarr_Plugin_nc7x_Example.js
@@ -0,0 +1,74 @@
+
+
+
+function details() {
+
+  return {
+    id: "Tdarr_Plugin_nc7x_Example",
+    Name: "No title meta data ",
+    Type: "Video",
+    Description: `This plugin removes metadata (if a title exists). The output container is the same as the original. \n\n
+`,
+    Version: "1.00",
+    Link: "https://github.com/HaveAGitGat/Tdarr_Plugin_nc7x_Example"
+  }
+
+}
+
+function plugin(file) {
+
+
+  //Must return this object
+
+  var response = {
+
+    processFile: false,
+    preset: '',
+    container: '.mp4',
+    handBrakeMode: false,
+    FFmpegMode: false,
+    reQueueAfter: false,
+    infoLog: '',
+
+  }
+
+  response.container = '.' + file.container
+  response.FFmpegMode = true
+
+
+  if (file.fileMedium !== "video") {
+
+
+    console.log("File is not video")
+
+    response.infoLog += " File is not video"
+    response.processFile = false;
+
+    return response
+
+  } else {
+
+   // var jsonString = JSON.stringify(file)
+
+
+    if (file.meta.Title != undefined) {
+
+      response.infoLog += " File has title metadata"
+      response.preset = ',-map_metadata -1 -c:v copy -c:a copy'
+      response.reQueueAfter = true;
+      response.processFile = true;
+      return response
+    } else {
+      response.infoLog += " File has no title metadata"
+    }
+
+
+    response.infoLog += " File meets conditions!"
+    return response
+
+  }
+}
+
+module.exports.details = details;
+
+module.exports.plugin = plugin;
